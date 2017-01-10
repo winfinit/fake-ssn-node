c3b9afb357e03cf908d49fb30866d78b4381cb8e
Roman Jurkov <winfinit@gmail.com>
2017-01-10 17:06:33 -0500

    progress

--- test/index.js
+++ test/index.js
@@ -3,20 +3,16 @@ var should = require('chai').should();
 let {ParseSSN, RandomSSN} = require('../build/ssn');
 
 describe('generate and validate', function() {
-    // for (var state in ssn.states) { 
-    //     var num = ssn.generate(ssn.states[state]);
-    //     it('ssn for ' + ssn.states[state], function() {
-    //         ssn.validate(num).should.equal(ssn.states[state]);
-    //     });
-    // }
 
     it('generates random social security number', function() {
     	let randomSSN = new RandomSSN();
+    	console.log(randomSSN.value().toString());
     	randomSSN.should.not.be.empty();
     });
 
-    it('generates random social security number', function() {
-    	let randomSSN = new RandomSSN();
-    	randomSSN.should.not.be.empty();
+    it('is parsing valid social security string', function() {
+    	let parseSSN = new ParseSSN('516386083');
+    	parseSSN.should.not.be.empty();
     });
+
 });
